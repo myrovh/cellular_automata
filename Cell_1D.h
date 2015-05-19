10d7e435c49b7d95a4c65fba540581a4eeb78cd6
myrovh <myrovh@yandex.com>
2015-05-19 21:35:45 +1000

    scrap everything start again

--- Cell_1D.h
+++ Cell_1D.h
@@ -8,26 +8,6 @@
 #include <string>
 
 class Cell_1D {
-public:
-    Cell_1D();
-
-    ~Cell_1D();
-
-    void swap_state();
-
-    bool seed_cell(std::string seed);
-
-    std::string get_current_grid_display();
-
-    int get_cell_length();
-
-private:
-    static const int length;
-    bool *cell_state_old;
-    bool *cell_state_new;
-    bool *current_state;
-    bool *future_state;
-    bool is_seeded;
 };
 
 #endif //CELL_1D_H
--- main.cpp
+++ main.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 #include "Cell_1D.h"
 
+std::string get_user_seed(Cell_1D *cell) {
+}
 
 int main() {
-    Cell_1D *test_cell = new Cell_1D();
-
-    std::cout << "Enter seed " << test_cell->get_cell_length() << " characters long for cell ('1' is positive value)" <<
-    std::endl;
-    std::string seed_value;
-    std::cin >> seed_value;
-    seed_value = seed_value.substr(0, 10);
-    test_cell->seed_cell(seed_value);
-
-    std::cout << test_cell->get_current_grid_display() << std::endl;
     return 0;
 }
